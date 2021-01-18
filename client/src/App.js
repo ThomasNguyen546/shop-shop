d679e8e0e503fc9621838811e789a143b469d38e
Thomas Nguyen <thomasnguyen546@gmail.com>
2021-01-17 18:44:20 -0700

    Strip Checkout

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
+import Success from "./pages/Success";
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -30,19 +31,21 @@ function App() {
       <Router>
         <div>
           <StoreProvider>
-            <Nav />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
-            </Switch>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/orderHistory" component={OrderHistory} />
+            <Route exact path="/products/:id" component={Detail} />
+            <Route exact path="/success" component={Success} />
+            <Route component={NoMatch} />
+          </Switch>
           </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
+
   );
 }
 
